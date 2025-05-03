647d52be45095533166b56c2b5ccb737293f705c
Daler Nazarov <architect0717@gmail.com>
2025-05-03 11:30:06 +0500

    Project edited!

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,27 @@
 const { Schema, default: mongoose } = require("mongoose");
 
 const UserSchema = new Schema({
-    name: {type: String},
-    surname: {type: String},
-    nationality: {type: String},    
-    birthYear: {type: Number}}, 
+    name: {
+        type: String,
+        minLength: 5,
+        maxLength: 30
+    },
+    surname: {
+        type: String,
+        minLength: 5,
+        maxLength: 30
+    },
+    nationality: {
+        type: String,
+        minLength: 5,
+        maxLength: 30  
+    },    
+    birthYear: {
+        type: Number,
+        min: [1950, "Please, relax. You should have a rest instead!"],
+        max: [2018, "You should be at least 7!"]
+    }
+}, 
     {
         timestamps: true,
     }    
